729e0124a9a2a64e802f7eb694a4cdf7cbb8facc
GianCalderon <gian.010692@gmail.com>
2019-12-16 18:58:50 -0500

    primer commit

---
+++ src/test/java/com/springboot/fixedTermVipAccount/SpringbootServicioAccountFixedTermVipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fixedTermVipAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioAccountFixedTermVipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
